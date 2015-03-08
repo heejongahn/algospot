0f72e0efbd1b2dfbe2e5c2d468d9894549fad2b7
Heejong Ahn <heejongahn@gmail.com>
2015-03-08 15:59:06 +0900

    Mercy(https://algospot.com/judge/problem/read/MERCY)

---
+++ mercy.py
@@ -0,0 +1,15 @@
+##### Problem
+# The administrators of algospot.com are so merciful, that they prepared really,
+# really easy problem to prevent contestants from frustration.
+
+##### Input
+# Input contains just one positive integer N(N <= 10).
+
+##### Output
+# Print N lines. Every line should contain 'Hello Algospot!'(quotation marks for
+# clarity) and nothing else.
+
+n = int(input("How many lines do you want to print?"))
+
+for i in range(n):
+    print ("Hello Algospot!")
